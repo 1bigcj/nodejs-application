5b49a2efc31ad30fb8a1773c17112fd128d74f9f
1bigcj <61260439+1bigcj@users.noreply.github.com>
2024-12-29 23:48:37 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,18 +1,25 @@
 const sonarqubeScanner = require('sonarqube-scanner');
+const fs = require('fs');
+const stream = require('stream');
+
+// Create a writable stream for logging
+const logStream = fs.createWriteStream('sonar_log.txt', { flags: 'a' });  // Logs to a file
+
+// Create a custom Console object that writes to the log file
+const logConsole = new console.Console({ stdout: logStream, stderr: logStream });
 
 sonarqubeScanner({
   serverUrl: 'http://54.167.26.94:9000/',
-       options : {
-	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample',
-	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '02cfc1523de1f1d203fd0cd8eda4ca9ce04346a3',
-	    //'sonar.password': 'admin',
-            'sonar.projectVersion':'1.0',
-	    'sonar.language':'js',
-            'sonar.sourceEncoding':'UTF-8',
-            'sonar.sources': '.',
-	  //'sonar.tests': 'specs',
-          //'sonar.inclusions' : 'src/**'
-       },
-}, () => {});
+  options: {
+    'sonar.projectDescription': 'This is a Node JS application',
+    'sonar.projectName': 'Node JS Application - Sample',
+    'sonar.projectKey': 'NodeJsLandmarkTechnologies',
+    'sonar.login': '02cfc1523de1f1d203fd0cd8eda4ca9ce04346a3',
+    'sonar.projectVersion': '1.0',
+    'sonar.language': 'js',
+    'sonar.sourceEncoding': 'UTF-8',
+    'sonar.sources': '.',
+  }
+}, () => {
+  logConsole.log('SonarQube scan finished.');
+});
